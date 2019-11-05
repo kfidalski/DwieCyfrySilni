7a7a0f45d7cb4dc432540400f4418510423f5230
kfidalski <48648696+kfidalski@users.noreply.github.com>
2019-11-05 11:40:18 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCTRL3
+
+{ 
+
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            string row = "";
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] array = new string[n];
+
+            for (int i = 0; i < n; i++) {
+
+                int m = Convert.ToInt32(Console.ReadLine());
+                long result = silnia(m);
+          
+
+            var len = result.ToString().Length;
+
+            var silniaString = result.ToString();
+
+
+
+
+            int l = 0;
+            l = Convert.ToInt32(len);
+
+            string j, d;
+
+            if (l == 1)
+            {
+                j = silniaString;
+                d = "0";
+                  
+                }
+            else
+            {
+                j = silniaString[len - 1].ToString();
+                d = silniaString[len - 2].ToString();
+                    
+                }
+
+                row = d + " " + j;
+                array[i] = row;
+                
+            //  Console.WriteLine($" Cyfry : {len} , d ={d} , j ={j}");
+            
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{array[i]}");
+            }
+            Console.ReadKey();
+        }
+
+        public static long  silnia(int n)
+        {
+            if(n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * silnia(n - 1);
+            }
+                
+        }
+
+    }
+}
